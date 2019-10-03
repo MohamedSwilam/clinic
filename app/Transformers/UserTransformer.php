b1d2014256d47ecd99a39f289ec524403bb0c1fe
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2019-10-03 20:46:50 +0200

    added login and index response

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @param User $user
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return $user->toArray();
+    }
+}
