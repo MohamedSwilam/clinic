beccacaeac56270110eb57816e1ae3b23fd1707e
Abdelrahman Rashwan <abdo.ra.1997@gmail.com>
2020-01-22 17:14:49 +0200

    added another statuses

--- config/status.php
+++ config/status.php
@@ -20,4 +20,68 @@ return [
         'name' => 'Missed',
         'color' => '#1E1E1E',
     ],
+    [
+        'name' => 'Bone',
+        'color' => '#001f3f',
+    ],
+    [
+        'name' => 'Implants',
+        'color' => '#0074D9',
+    ],
+    [
+        'name' => 'Porcelain Crowns',
+        'color' => '#7FDBFF',
+    ],
+    [
+        'name' => 'Emax Crown',
+        'color' => '#39CCCC',
+    ],
+    [
+        'name' => 'Endo',
+        'color' => '#3D9970',
+    ],
+    [
+        'name' => 'Filling',
+        'color' => '#2ECC40',
+    ],
+    [
+        'name' => 'Scaling  Polishing',
+        'color' => '#01FF70',
+    ],
+    [
+        'name' => 'Whittening',
+        'color' => '#FF851B',
+    ],
+    [
+        'name' => 'Gum',
+        'color' => '#FF4136',
+    ],
+    [
+        'name' => 'Extraction',
+        'color' => '#85144b',
+    ],
+    [
+        'name' => 'Denture',
+        'color' => '#F012BE',
+    ],
+    [
+        'name' => 'X-ray',
+        'color' => '#111111',
+    ],
+    [
+        'name' => 'Panorama',
+        'color' => '#B10DC9',
+    ],
+    [
+        'name' => 'Ct 1/4 arch',
+        'color' => '#AAAAAA',
+    ],
+    [
+        'name' => 'Ct Arch',
+        'color' => '#DDDDDD',
+    ],
+    [
+        'name' => 'Ct Both Arches',
+        'color' => '#2E2E2E',
+    ],
 ];
